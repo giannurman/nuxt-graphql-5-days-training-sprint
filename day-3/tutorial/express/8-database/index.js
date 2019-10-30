623cee25bddbbc932486cf24c008c78293c72e37
Gian Nurman Lestari <giannurman10@gmail.com>
2019-10-30 13:21:30 +0700

    ExpressJS -  Database

---
+++ day-3/tutorial/express/8-database/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
+
+var upload = multer();
+var app = express();
+
+var personSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  nationality: String
+});
+var Person = mongoose.model("Person", personSchema);
+
+app.get('/person', function(req, res){
+  res.render('person');
+});
+
+app.post('/person', function(req, res){
+  var personInfo = req.body; //Get the parsed information
+  
+  if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+     res.render('show_message', {
+        message: "Sorry, you provided worng info", type: "error"});
+  } else {
+     var newPerson = new Person({
+        name: personInfo.name,
+        age: personInfo.age,
+        nationality: personInfo.nationality
+     });
+   
+     newPerson.save(function(err, Person){
+        if(err)
+           res.render('show_message', {message: "Database error", type: "error"});
+        else
+           res.render('show_message', {
+              message: "New person added", type: "success", person: personInfo});
+     });
+  }
+});
+
+app.listen(3000);
