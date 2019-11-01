5188f365947b3c59f52f521604e7eabf93bc7e12
Gian Nurman Lestari <giannurman10@gmail.com>
2019-11-01 11:24:26 +0700

    Install/Configure the Required Modules, Dependencies, and Router

--- day-5/tutorial/client/src/main.js
+++ day-5/tutorial/client/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+import VueRouter from 'vue-router'
+import router from './router'
 
 Vue.config.productionTip = false
 
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.use(VueApollo);
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App),
+  apolloProvider,
+  router,
+  render: h => h(App)
 }).$mount('#app')
