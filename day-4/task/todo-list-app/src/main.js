bad8a367010886b82728d495768862f00ba0ba11
Gian Nurman Lestari <giannurman10@gmail.com>
2019-11-04 10:42:16 +0700

    Working graphql vue app

--- day-4/task/todo-list-app/src/main.js
+++ day-4/task/todo-list-app/src/main.js
@@ -2,10 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
 Vue.config.productionTip = false
 
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.use(VueApollo);
+
 new Vue({
   vuetify,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
